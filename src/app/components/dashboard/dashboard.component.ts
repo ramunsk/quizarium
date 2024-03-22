384c9212559df1370d92b78794cc5c15d35aa70f
Ramunas <514899+ramunsk@users.noreply.github.com>
2024-03-22 20:15:40 +0200

    Dashboard improvements

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'qz-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'qz-dashboard',
+    standalone: true,
+    imports: [],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-
+    startQuiz(): void {
+        console.log("Let's go!");
+    }
 }
