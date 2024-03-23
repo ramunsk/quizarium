c032e78af42f48edbc8e5639d6b557b29b002e1c
Ramunas <514899+ramunsk@users.noreply.github.com>
2024-03-23 19:57:05 +0200

    Mapping questions from fake API to models

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,12 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, take } from 'rxjs';
+import { Observable, map, take } from 'rxjs';
+import { Answer } from '../model/answer';
+import { Question } from '../model/question';
+
+interface QuestionJson {
+    question: string;
+    choices: string[];
+    correctAnswer: number;
+}
 
 @Injectable({ providedIn: 'root' })
 export class QuizService {
-    constructor(private http: HttpClient) {}
+    private _questions: Question[];
+
+    constructor(private http: HttpClient) {
+        this._questions = [];
+    }
 
     loadQuestions(): Observable<void> {
-        return this.http.get<void>('assets/questions.json').pipe(take(1));
+        return this.http.get<QuestionJson[]>('assets/questions.json').pipe(
+            take(1),
+            map((questions: QuestionJson[]) => {
+                this._questions = questions.map((json) => {
+                    const answers = json.choices.map((choice, i) => new Answer(choice, i === json.correctAnswer));
+                    return new Question(json.question, answers);
+                });
+            })
+        );
     }
 }
